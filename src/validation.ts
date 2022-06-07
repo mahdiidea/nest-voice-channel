e3b25ce8689cbb2a8f78b46622725fe5b8c8d5bc
mahdi <96rajabi@gmail.com>
2022-06-07 12:43:09 +0430

    added custom validation response schema

--- src/validation.ts
+++ src/validation.ts
@@ -12,14 +12,21 @@ export class ValidationPipe implements PipeTransform<any> {
             const errors = await validate(object)
 
             if (errors.length > 0) {
-                  const errorsResponse: any = errors.map((val: any) => {
-                        return {
-                              code: 400, errors: val
-                        }
+                  let array = {};
+                  let raw = [];
+                  for (let i = 0; i < errors.length; i++) {
+                        array[errors[i].property] = Object.keys(errors[i].constraints).map(function (key) {
+                              raw.push(errors[i].constraints[key]);
+                              return errors[i].constraints[key];
+                        });
+                  }
+
+                  throw new BadRequestException({
+                        message: raw.length > 0 ? (raw.length > 1 ? `${raw[0]}. (and ${raw.length - 1} more ${raw.length == 2 ? 'error' : 'errors'})` : raw[0]) : 'Bad request',
+                        errors: array
                   })
-                  throw new BadRequestException({ errors: errorsResponse })
             }
-            return value
+            return value;
       }
 
       private toValidate(metatype: Function): boolean {
